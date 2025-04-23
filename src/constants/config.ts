d5aa99b2437b442cca53c59cb8136d2edb8aba80
ishangiri <ishansmith2@gmail.com>
2025-04-23 19:30:44 +1000

    made some changes

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,13 +40,13 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Ishan Giri — 3D Portfolio",
+    fullName: "Ishan Giri",
+    email: "ishansmith2@gmail.com",
   },
   hero: {
-    name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+    name: "Ishan Giri",
+    p: ["I am a full stack developer.", "I create web applications and mobile applications.",],
   },
   contact: {
     p: "Get in touch",
@@ -67,9 +67,9 @@ export const config: TConfig = {
     about: {
       p: "Introduction",
       h2: "Overview.",
-      content: `I'm a skilled software developer with experience in TypeScript and
+      content: `I'm a skilled full stack software developer with experience in TypeScript and
       JavaScript, and expertise in frameworks like React, Node.js, and
-      Three.js. I'm a quick learner and collaborate closely with clients to
+     react-native. I'm a quick learner and collaborate closely with clients to
       create efficient, scalable, and user-friendly solutions that solve
       real-world problems. Let's work together to bring your ideas to life!`,
     },
